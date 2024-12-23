d23695629eb0376b799bb8987a74368ddd309cc5
bvuong19 <vuongbrian3@gmail.com>
2024-12-23 14:49:34 -0800

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "zovqfezlcqmrkqpkasli.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
